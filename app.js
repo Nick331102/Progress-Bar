a2174c6a06eb32580bfe00bbb71f4226c368a22d
Nick331102 <75483104+Nick331102@users.noreply.github.com>
2021-12-15 19:57:44 -0500

    Add files via upload
    JS fixed for progress bar width

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ function update() {
         }
     })
 
-    const actives = docuemnt.querySelectorAll(".active");
+    const actives = document.querySelectorAll(".active");
 
     progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + "%";
 
